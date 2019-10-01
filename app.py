97b73869334942dce1d690138dcfeacf2cc0004e
soundreaper@gmail.com <soundreaper@gmail.com>
2019-10-01 10:29:51 -0700

    add basic templates

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     """Return homepage."""
-    return 'Hello, world!'
+    return render_template('home.html', msg='Flask is Cool!!')
 
 if __name__ == '__main__':
     app.run(debug=True)
